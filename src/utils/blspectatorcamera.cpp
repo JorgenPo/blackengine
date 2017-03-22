76a30ec484798bf8cfbe0a31ac604c7668ef4100
George Popoff <popoff96@live.com>
2017-03-22 13:41:03 +0300

    fps spectator camera class v1.0

--- src/utils/blspectatorcamera.cpp
+++ src/utils/blspectatorcamera.cpp
@@ -1,6 +1,65 @@
 #include "blspectatorcamera.h"
 
-BlSpectatorCamera::BlSpectatorCamera()
+#include <cmath>
+#include <bllogger.h>
+
+#include <QCursor>
+
+namespace black {
+
+SpectatorCamera::SpectatorCamera()
+    : Camera(), m_lastMousePos(), m_firstMouseEvent()
+{
+    this->setPosition({0.0, 5.0f, 20.0f});
+
+    this->setPitchConstraint(90);
+    this->setYawConstraint(90);
+}
+
+void SpectatorCamera::handleKeyboard(QKeyEvent *e)
+{
+    int speed = 10.0f;
+    switch ( e->key() ) {
+    case Qt::Key_Left:
+    case Qt::Key_A:
+        this->moveLeft(speed);
+        break;
+    case Qt::Key_Right:
+    case Qt::Key_D:
+        this->moveRight(speed);
+        break;
+    case Qt::Key_Up:
+    case Qt::Key_W:
+        this->moveForward(speed);
+        break;
+    case Qt::Key_Down:
+    case Qt::Key_S:
+        this->moveBack(speed);
+        break;
+    default:
+        break;
+    }
+}
+
+void SpectatorCamera::handleMouse(QMouseEvent *e)
+{
+    if ( !m_firstMouseEvent ) {
+        float dx = QCursor::pos().x() - m_lastMousePos.x();
+        float dy = m_lastMousePos.y() - QCursor::pos().y();
+        dx *= 0.5f;
+        dy *= 0.5f;
+
+        this->setRotate({m_pitch + dy, m_yaw + dx, m_roll});
+    } else {
+        m_firstMouseEvent = false;
+    }
+
+    m_lastMousePos = QCursor::pos();
+}
+
+void SpectatorCamera::handleWheel(QWheelEvent *e)
 {
 
 }
+
+} // end of black namespace
--- src/utils/blspectatorcamera.h
+++ src/utils/blspectatorcamera.h
@@ -1,11 +1,27 @@
 #ifndef BLSPECTATORCAMERA_H
 #define BLSPECTATORCAMERA_H
 
+#include "src/core/blcamera.h"
+#include <QPoint>
 
-class BlSpectatorCamera
+namespace black {
+
+class SpectatorCamera : public Camera
 {
 public:
-    BlSpectatorCamera();
+    SpectatorCamera();
+
+    // Camera interface
+public:
+    void handleKeyboard(QKeyEvent *e) override;
+    void handleMouse(QMouseEvent *e) override;
+    void handleWheel(QWheelEvent *e) override;
+
+private:
+    QPointF m_lastMousePos;
+    bool m_firstMouseEvent;
 };
+
+} // end of black namespace
 
 #endif // BLSPECTATORCAMERA_H
