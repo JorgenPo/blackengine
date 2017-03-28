eb95f8875f6a1af585f6058298d11577cab02060
George Popoff <popoff96@live.com>
2017-03-28 12:00:31 +0300

    shader class v1.0  Just a thin wrapper around a QOpenGLShader class  Can be handeled by Resource Manager

---
+++ src/core/blshader.h
@@ -0,0 +1,54 @@
+#ifndef BLSHADER_H
+#define BLSHADER_H
+
+#include <src/core/blresourcemanager.h>
+#include <src/core/blresource.h>
+
+#include <QOpenGLShader>
+
+#include <string>
+
+namespace black {
+
+class ResourceManager;
+class ShaderProgram;
+
+/**
+ * @brief The Shader class
+ * Just a slim wrapper around a QOpenGLShader class.
+ * It's purpose generaly is to make possible to handle shaders
+ * by Resource Manager class.
+ *
+ * @version 1.0 27.03.2017
+ * First working version.
+ *
+ * @author george popof <popoff96@live.com>
+ * @resource
+ */
+class Shader : public Resource
+{
+    friend class ResourceManager;
+    friend class ShaderProgram;
+public:
+    Shader();
+    Shader(std::string file);
+
+    virtual ~Shader();
+
+    bool isCompiled();
+    std::string log() const;
+    QOpenGLShader::ShaderType shaderType() const;
+
+protected:
+    QOpenGLShader* shader();
+
+    // Resource interface
+private:
+    void load(std::string file) override;
+
+private:
+    QOpenGLShader* m_shader;
+};
+
+} // end of black namespace
+#endif // BLSHADER_H
