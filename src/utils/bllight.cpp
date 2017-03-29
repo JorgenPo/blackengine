70334b77b79c0841da4d4e7bc5cff35eecc2d801
George Popoff <popoff96@live.com>
2017-03-30 02:21:55 +0300

    Reworked version of light class v1.0a  - All components of light now lies separately  - Added shine factor

--- src/utils/bllight.cpp
+++ src/utils/bllight.cpp
@@ -3,16 +3,20 @@
 namespace black {
 
 Light::Light()
-    : m_position(),
-      m_color()
+    : m_position(0, 10.0f, 0),
+      m_ambient(0.1f, 0.1f, 0.1f),
+      m_diffuse(0.8f, 0.8f, 0.8f),
+      m_spectacular(1.0f, 1.0f, 1.0f)
 {
 
 }
 
-Light::Light(std::array<float, 3> lightPos,
-             std::array<float, 3> lightColor)
-    : m_position(lightPos[0], lightPos[1], lightPos[2]),
-      m_color(lightColor[0], lightColor[1], lightColor[2])
+Light::Light(const QVector3D& lightPos,
+             const QVector3D& ambient,
+             const QVector3D& diffuse,
+             const QVector3D& spectacular)
+    : m_position(lightPos), m_ambient(ambient),
+      m_diffuse(diffuse), m_spectacular(spectacular)
 {
 
 }
@@ -22,19 +26,40 @@ QVector3D Light::position() const
     return m_position;
 }
 
+QVector3D Light::ambient() const
+{
+    return m_ambient;
+}
+
+QVector3D Light::diffuse() const
+{
+    return m_diffuse;
+}
+
+QVector3D Light::spectacular() const
+{
+    return m_spectacular;
+}
+
 void Light::setPosition(const QVector3D &position)
 {
     m_position = position;
 }
 
-QVector3D Light::color() const
+void Light::setAmbient(const QVector3D &ambient)
 {
-    return m_color;
+    m_ambient = ambient;
 }
 
-void Light::setColor(const QVector3D &color)
+void Light::setDiffuse(const QVector3D &diffuse)
 {
-    m_color = color;
+    m_diffuse = diffuse;
 }
+
+void Light::setSpectacular(const QVector3D &spectacular)
+{
+    m_spectacular = spectacular;
+}
+
 
 }
--- src/utils/bllight.h
+++ src/utils/bllight.h
@@ -11,6 +11,10 @@ namespace black {
  *
  * @brief The Light object class
  *
+ * @version 1.0a 29.03.2017
+ * Reworked version. All components stores separetely.
+ * Added shine factor.
+ *
  * @version 1.0 First version
  *
  * @author george popoff <popoff96@live.com>
@@ -21,18 +25,25 @@ class Light
 public:
     Light();
 
-    Light(std::array<float, 3> lightPos,
-          std::array<float, 3> lightColor);
+    Light(const QVector3D& lightPos,
+          const QVector3D& ambient,
+          const QVector3D& diffuse,
+          const QVector3D& spectacular);
 
     QVector3D position() const;
-    QVector3D color() const;
+    QVector3D ambient() const;
+    QVector3D diffuse() const;
+    QVector3D spectacular() const;
 
     void setPosition(const QVector3D &position);
-    void setColor(const QVector3D &color);
-
+    void setAmbient(const QVector3D &ambient);
+    void setDiffuse(const QVector3D &diffuse);
+    void setSpectacular(const QVector3D &spectacular);
 private:
     QVector3D m_position;
-    QVector3D m_color;
+    QVector3D m_ambient;
+    QVector3D m_diffuse;
+    QVector3D m_spectacular;
 };
 
 }
