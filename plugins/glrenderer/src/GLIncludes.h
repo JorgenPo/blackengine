0d824574804e76564796ccc928db12870eed14c0
George Popoff <popoff96@live.com>
2018-04-22 18:58:36 +0300

    Add common includes file for gl render plugin

---
+++ plugins/glrenderer/src/GLIncludes.h
@@ -0,0 +1,11 @@
+//
+// Created by popof on 22.04.2018.
+//
+
+#ifndef BLACKENGINE_GLINCLUDES_H
+#define BLACKENGINE_GLINCLUDES_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#endif //BLACKENGINE_GLINCLUDES_H
