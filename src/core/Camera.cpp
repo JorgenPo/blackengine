e904c79b1a54885421122d55efb33e62d88d815e
George Popoff <popoff96@live.com>
2018-05-01 17:07:29 +0300

    Remove camera component, Camera class only left

--- src/core/Camera.cpp
+++ src/core/Camera.cpp
@@ -2,10 +2,138 @@
 // Created by popof on 30.04.2018.
 //
 
+#include <glm/gtc/matrix_transform.hpp>
+#include <core/Core.h>
 #include "Camera.h"
 
 namespace black {
-    Camera::Camera(glm::vec4 viewport) {
-        this->camera = std::make_shared<components::CameraComponent>(viewport);
+
+    Camera::Camera(glm::vec4 viewport, glm::vec3 position, glm::vec3 up, float pitch, float yaw, float roll)
+        : up(up), movementSpeed(5.0f), softness(2.0f), zoom(1.0f), viewport(viewport)
+    {
+        this->worldUp = up;
+        this->fov = 45.0f;
+        this->fovMin = 1.0f;
+        this->fovMax = fov;
+
+        this->nearPlane = 0.1f;
+        this->farPlane = 100.0f;
+        this->projectionType = ProjectionType::PERSPECTIVE;
+
+        this->aspectRatio = 16.0f / 9.0f;
+
+        this->transform->setPosition(position);
+        this->transform->setRotation(glm::vec3{pitch, yaw, roll});
+    }
+
+    float Camera::getMovementSpeed() const {
+        return movementSpeed;
+    }
+
+    void Camera::setMovementSpeed(float movementSpeed) {
+        Camera::movementSpeed = movementSpeed;
+    }
+
+    float Camera::getSoftness() const {
+        return softness;
+    }
+
+    void Camera::setSoftness(float softness) {
+        Camera::softness = softness;
+    }
+
+    float Camera::getZoom() const {
+        return zoom;
+    }
+
+    void Camera::setZoom(float zoom) {
+        Camera::zoom = zoom;
+        updateProjectionMatrix();
+    }
+
+    glm::mat4 Camera::getProjectionMatrix() {
+        return this->projection;
+    }
+
+    glm::mat4 Camera::getViewMatrix() {
+        return this->view;
+    }
+
+    void Camera::setProjectionType(ProjectionType projectionType) {
+        Camera::projectionType = projectionType;
+    }
+
+    float Camera::getAspectRatio() const {
+        return aspectRatio;
+    }
+
+    void Camera::setAspectRatio(float aspectRatio) {
+        Camera::aspectRatio = aspectRatio;
+    }
+
+    float Camera::getFov() const {
+        return fov;
+    }
+
+    void Camera::setFov(float fov) {
+        Camera::fov = fov;
+    }
+
+    float Camera::getNearPlane() const {
+        return nearPlane;
+    }
+
+    void Camera::setNearPlane(float nearPlane) {
+        Camera::nearPlane = nearPlane;
+    }
+
+    float Camera::getFarPlane() const {
+        return farPlane;
+    }
+
+    void Camera::setFarPlane(float farPlane) {
+        Camera::farPlane = farPlane;
+    }
+
+    void Camera::updateProjectionMatrix() {
+        if (projectionType == ProjectionType::PERSPECTIVE) {
+            float zoomedFov = this->fov / this->zoom;
+            if (zoomedFov < this->fovMin) zoomedFov = this->fovMin;
+            if (zoomedFov > this->fovMax) zoomedFov = this->fovMax;
+
+            this->projection = glm::perspective(glm::radians(zoomedFov), this->aspectRatio,
+                    this->nearPlane, this->farPlane);
+        } else {
+            this->projection = glm::ortho(viewport.x, viewport.z, viewport.y, viewport.w, nearPlane, farPlane);
+        }
+    }
+
+    void Camera::updateViewMatrix() {
+        float pitch = this->transform->getRotation().x;
+        float yaw = this->transform->getRotation().y;
+
+        glm::vec3 front;
+        front.x = static_cast<float>(cos(glm::radians(yaw)) * cos(glm::radians(pitch)));
+        front.y = static_cast<float>(sin(glm::radians(pitch)));
+        front.z = static_cast<float>(sin(glm::radians(yaw)) * cos(glm::radians(pitch)));
+
+        this->front = glm::normalize(front);
+        this->right = glm::normalize(glm::cross(this->front, this->worldUp));
+        this->up = glm::normalize(glm::cross(this->right, this->front));
+
+        this->view = glm::lookAt(this->transform->getPosition(), this->transform->getPosition() + this->front, this->up);
+    }
+
+    const glm::vec4 &Camera::getViewport() const {
+        return viewport;
+    }
+
+    void Camera::setViewport(const glm::vec4 &viewport) {
+        Camera::viewport = viewport;
+    }
+
+    void Camera::updateCamera() {
+        this->updateViewMatrix();
+        this->updateProjectionMatrix();
     }
 }
--- src/core/Camera.h
+++ src/core/Camera.h
@@ -2,24 +2,102 @@
 // Created by popof on 30.04.2018.
 //
 
-#ifndef BLACKENGINE_CAMERA_H
-#define BLACKENGINE_CAMERA_H
+#ifndef BLACKENGINE_CAMERACOMPONENT_H
+#define BLACKENGINE_CAMERACOMPONENT_H
 
-#include <core/components/CameraComponent.h>
-#include "GameEntity.h"
+#include <glm/glm.hpp>
+#include <core/GameEntity.h>
+#include "core/components/Component.h"
 
 namespace black {
 
+    enum class ProjectionType {
+        ORTHOGRAPHIC,
+        PERSPECTIVE
+    };
+
     /**
-     * Camera entity
+     * Camera component class
      */
     class Camera : public GameEntity {
-    public:
-        std::shared_ptr<components::CameraComponent> camera;
+    protected:
+        glm::vec3 worldUp;
+        glm::vec3 up;
+        glm::vec3 front;
+        glm::vec3 right;
 
-        explicit Camera(glm::vec4 viewport);
+        float movementSpeed;
+        float softness;
+        float zoom;
+
+        glm::mat4 view;
+        glm::mat4 projection;
+        ProjectionType projectionType;
+
+        float aspectRatio;
+
+        float fov;
+        float fovMin;
+        float fovMax;
+
+        float nearPlane;
+        float farPlane;
+
+        glm::vec4 viewport;
+    public:
+        explicit Camera(glm::vec4 viewport = glm::vec4(0.0f), glm::vec3 position = {0.0f, 0.0f, 0.0f},
+                glm::vec3 up = {0.0f, 1.0f, 0.0f},
+                float pitch = 0.0f, float yaw = -90.0f, float roll = 0.0f);
+
+        float getMovementSpeed() const;
+
+        void setMovementSpeed(float movementSpeed);
+
+        float getSoftness() const;
+
+        void setSoftness(float softness);
+
+        float getZoom() const;
+
+        void setZoom(float zoom);
+
+        glm::mat4 getProjectionMatrix();
+
+        glm::mat4 getViewMatrix();
+
+        void setProjectionType(ProjectionType projectionType);
+
+        float getAspectRatio() const;
+
+        void setAspectRatio(float aspectRatio);
+
+        float getFov() const;
+
+        void setFov(float fov);
+
+        float getNearPlane() const;
+
+        void setNearPlane(float nearPlane);
+
+        float getFarPlane() const;
+
+        void setFarPlane(float farPlane);
+
+        const glm::vec4 &getViewport() const;
+
+        void setViewport(const glm::vec4 &viewport);
+
+        /**
+         * Updates a camera. Concrete subclasses should set this
+         */
+        virtual void updateCamera();
+
+        virtual Camera *copy() = 0;
+    private:
+        void updateProjectionMatrix();
+        void updateViewMatrix();
     };
 }
 
 
-#endif //BLACKENGINE_CAMERA_H
+#endif //BLACKENGINE_CAMERACOMPONENT_H
