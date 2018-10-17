1c654d465dbee418495f8ff0c8675e327aea3c64
George Popoff <popoff96@live.com>
2018-10-17 23:59:20 +0300

    Update CMakeLists and example

--- src/examples/initialization/src/main.cpp
+++ src/examples/initialization/src/main.cpp
@@ -2,23 +2,29 @@
 // Created by popof on 24.09.2018.
 //
 #include <iostream>
+#include <memory>
 #include <Engine.h>
 #include <application/GameApplication.h>
+#include <util/Meshes.h>
 
 using namespace black;
 
 class BlackEngineApplication : public GameApplication {
+    std::shared_ptr<Mesh> triangleMesh;
+
 public:
     BlackEngineApplication() : GameApplication(std::string("BlackEngineApplication") + Constants::RuntimePlatformString,
             800, 600, false) {
-        //this->logger->info("Starting initialization example");
+
     }
 
 private:
     void update() override {
+        this->renderer->render(this->triangleMesh);
     }
 
     void initializeResources() override {
+        this->triangleMesh = Meshes::create({1.0f, 1.0f, 0.0f});
     }
 };
 
