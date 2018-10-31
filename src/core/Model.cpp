46571ba74464279a47b15cfe51735b1cdf070380
George Popoff <popoff96@live.com>
2018-10-31 22:44:19 +0300

    Add stub version of the Model class.  - Model is a list of parts (meshes with a custom materials)

---
+++ src/core/Model.cpp
@@ -0,0 +1,37 @@
+#include <utility>
+
+//
+// Created by popof on 28.10.2018.
+//
+
+#include "Model.h"
+
+namespace black {
+
+    const std::list<ModelPart> &Model::getParts() const {
+        return parts;
+    }
+
+    Model::Model(std::list<ModelPart> parts) : parts(std::move(parts)) {
+
+    }
+
+    const ModelPart &Model::getPart(std::string name) {
+        auto foundPart = std::find_if(this->parts.begin(), this->parts.end(), [name](const auto &part) {
+            return part.name == name;
+        });
+
+        if (foundPart == this->parts.end()) {
+            throw ModelPartNotFoundException(name);
+        }
+
+        return *foundPart;
+    }
+
+    ModelPart::ModelPart(const std::string &name, const std::shared_ptr<Mesh> &mesh,
+                         const std::shared_ptr<Material> &material) : name(name), mesh(mesh), material(material) {}
+
+    ModelPartNotFoundException::ModelPartNotFoundException(std::string partName) : Exception() {
+        this->message << "Model part with name '" << partName << "' does not exist" << std::endl;
+    }
+}
---
+++ src/core/Model.h
@@ -0,0 +1,61 @@
+//
+// Created by popof on 28.10.2018.
+//
+
+#ifndef BLACKENGINE_MODEL_H
+#define BLACKENGINE_MODEL_H
+
+#include <CommonHeaders.h>
+#include <Mesh.h>
+#include <Material.h>
+
+namespace black {
+
+    class BLACK_EXPORTED ModelPartNotFoundException : public Exception {
+    public:
+        explicit ModelPartNotFoundException(std::string partName);
+    };
+
+    /**
+     * Part of a model. One mesh and material with a name.
+     */
+    struct BLACK_EXPORTED ModelPart {
+        std::string name;
+        std::shared_ptr<Mesh> mesh;
+        std::shared_ptr<Material> material;
+
+        ModelPart(const std::string &name, const std::shared_ptr<Mesh> &mesh,
+                  const std::shared_ptr<Material> &material);
+    };
+
+    /**
+     * One or more meshes with some material each
+     */
+    class BLACK_EXPORTED Model {
+        std::list<ModelPart> parts;
+
+    public:
+        /**
+         * Construct a model from model parts
+         */
+        explicit Model(std::list<ModelPart> parts);
+
+        /**
+         * Return a list of the model parts
+         *
+         * @return List of model parts
+         */
+        const std::list<ModelPart> &getParts() const;
+
+        /**
+         * Get the model part with a specific name.
+         *
+         * @param name Name of the model part
+         * @return Part of the model corresponding a given name
+         * @throws ModelPartNotFoundException If ModelPart with a given name has not been found
+         */
+        const ModelPart &getPart(std::string name);
+    };
+
+}
+#endif //BLACKENGINE_MODEL_H
